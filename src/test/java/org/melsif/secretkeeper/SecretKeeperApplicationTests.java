b28424b382fc82f2cd17623cd5eb5ad4be19e815
lefilou <fliousalim@gmail.com>
2020-09-29 23:50:08 -0400

    Initial commit

---
+++ src/test/java/org/melsif/secretkeeper/SecretKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.melsif.secretkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
