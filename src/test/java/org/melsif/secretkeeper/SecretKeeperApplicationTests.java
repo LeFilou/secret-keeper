9743770e34ce1fe969f66dc9fdaca16bf35c228b
lefilou <fliousalim@gmail.com>
2020-10-08 00:15:53 -0400

    configure liquibase

--- src/test/java/org/melsif/secretkeeper/SecretKeeperApplicationTests.java
+++ src/test/java/org/melsif/secretkeeper/SecretKeeperApplicationTests.java
@@ -2,8 +2,10 @@ package org.melsif.secretkeeper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SecretKeeperApplicationTests extends AbstractIntegrationTest {
 
     @Test
